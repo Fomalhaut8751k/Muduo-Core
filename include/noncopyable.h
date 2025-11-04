25498bf817a9f6a53fd15410a788dc9a5959da2d
Fomalhaut8751k <1308041515@qq.com>
2025-11-04 21:38:12 +0800

    fifth commit

---
+++ include/noncopyable.h
@@ -0,0 +1,28 @@
+#ifndef NONCOPYABLE_H
+#define NONCOPYABLE_H
+
+namespace muduo
+{
+    class noncopyable
+    {
+    public:
+        /*
+            派生类的拷贝构造的时候需要先调用基类的拷贝构造
+            但是基类的拷贝构造被删除了，因此派生类也无法拷贝构造
+            等号赋值也一样。
+        */
+        noncopyable(const noncopyable&) = delete;
+        noncopyable& operator=(const noncopyable&) = delete;
+
+    protected:
+        /*
+            派生类的构造函数调用时会先调用基类的构造函数
+            私有继承下，派生类依然可以范围基类保护权限下的方法
+        */
+       noncopyable() = default;
+       ~noncopyable() = default;
+    };
+}
+
+
+#endif
